cf5ddde1a7f7214ae5606b2bd39041e62fe2265c
Rafael Fernandes <rafael.londrina@gmail.com>
2022-05-10 17:01:12 -0300

    configuracao email no nodemailer e envio para mailtrap

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
 import { prisma } from './prisma';
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "4cf9996e1e5b92",
+    pass: "a15f4645764c09"
+  }
+});
 
 app.post('/feedbacks', async (req, res) => {
 
@@ -20,8 +30,22 @@ app.post('/feedbacks', async (req, res) => {
     }
 })
 
+await transport.sendMail({
+  from: 'Equipe Feedget <oi@feedget.com>',
+  to: 'Rafael Fernandes <rafael.londrina@gmail.com>',
+  subject: 'Novo feedback',
+   /** Colocar dentro de um array para que nao conte espaço em branco */
+  html: [
+    `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+    `<p>Tipo de feedback: ${type}</p>`,
+    `<p>Comentário: ${comment}</p>`,	
+    `</div>`,
+  ].join('\n')
+});
+
 return res.status(201).json({ data: feedback });
 });
+
 
 app.listen(3333, () => {
   console.log('Server started on port 3333');
